386e8dad4ccf9b3458248b6e5705fefb99385b78
“Reyhan <rcteenx@gmail.com>
2024-06-17 22:08:10 +0300

    add hamburger menu

--- components/ui/header/ServiceNav.jsx
+++ components/ui/header/ServiceNav.jsx
@@ -1,10 +1,20 @@
-import IconComponent from "@/components/ui/icons/icons";
+"use client";
+import Link from "next/link";
+import { StaticPages } from "@/content/data";
+
+import { useState } from "react"; // import state
 
 export default function MobileMenu() {
+  const menu = StaticPages.filter((m) => m.id < 6);
+  const [isNavOpen, setIsNavOpen] = useState(false);
+  function handleClick() {
+    setIsNavOpen((prev) => !prev);
+  }
+
   return (
     <div className="w-48 px-4 flex justify-end">
       <nav id="service-menu">
-        <ul className="flex space-x-4 text-sm m-4">
+        <ul className="hidden xl:flex space-x-4 text-sm m-4">
           <li className="gradientAccentToDark gradientTransition text-white rounded-xl py-1 px-3">
             <a href="/blog">Blog</a>
           </li>
@@ -12,6 +22,47 @@ export default function MobileMenu() {
             <a href="/iletisim">İletişim</a>
           </li>
         </ul>
+
+        <section className="flex xl:hidden cursor-pointer">
+          <div className="space-y-2 h-8 w-8" onClick={handleClick}>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+            <span className="block h-0.5 w-8 animate-pulse bg-gray-600"></span>
+          </div>
+
+          <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+            <div
+              className="CROSS-ICON absolute top-0 right-0 px-8 py-8"
+              onClick={() => setIsNavOpen(false)}
+            >
+              <svg
+                className="h-8 w-8 text-red-600"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1="18" y1="6" x2="6" y2="18" />
+                <line x1="6" y1="6" x2="18" y2="18" />
+              </svg>
+            </div>
+
+            <ul className="MENU-LINK-MOBILE-OPEN flex flex-col items-center justify-between min-h-[250px]">
+              {menu.map((p) => (
+                <li
+                  key={p.id}
+                  className={`border-b border-gray-400 my-8 uppercase`}
+                >
+                  <Link href={`/${p.link}`} onClick={handleClick}>
+                    {p.title.toUpperCase()}
+                  </Link>
+                </li>
+              ))}
+            </ul>
+          </div>
+        </section>
       </nav>
     </div>
   );
