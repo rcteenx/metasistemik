d465442a0a6d4769870b1db8253b730bd503e9d5
“Reyhan <rcteenx@gmail.com>
2024-06-26 14:11:51 +0300

    update home sub 1

--- components/sections/home/1b-sub-hero.jsx
+++ components/sections/home/1b-sub-hero.jsx
@@ -2,7 +2,7 @@ export default function SubHero({ img }) {
   return (
     <section id="subhero" className="container mx-auto px-4 my-16 md:p-0 ">
       <h2 className="p-4 lg:p-8 text-center lg:text-6xl gradientAccentToDark sm:rounded-xl ">
-        METASİSTEMİK ÇALIŞMA NEDİR?
+        METASİSTEMİK YAKLAŞIMI KEŞFEDİN
       </h2>
       <div className="grid grid-cols-1 xl:grid-cols-2 gap-8 lg:mt-4 justify-center items-center">
         <div className="flex justify-center items-center p-4 rounded-xl bg-black">
@@ -20,17 +20,17 @@ export default function SubHero({ img }) {
         <div className="px-4 text-center xl:text-left ">
           <div className="lg:my-8 text-base [&>p]:leading-6 [&>p]:my-4">
             <p>
-              Farkındalık Geliştirme ve Sistemik Çalışmaya olan bağlılığımıza
-              dayalı bir çalışma sistemidir.
-            </p>
-            <p>
-              Kişisel farkındalığı geliştirmenin ve bütünsel birbirine bağlılığı
-              anlamak; bireylerde ve ekiplerde dönüşüm potansiyelinin kilidini
-              açmak için gereklidir.
+              Bireysel ve Kurumsal Dönüşümün Anahtarı: Platformumuz bünyesinde
+              ye alan okullarımız, farkındalık geliştirme ve sistemik çalışma
+              anlayışına bağlı kalarak ve global standartlara uyumluluğu
+              sağlayarak, bireylerin ve organizasyonların potansiyellerini
+              keşfetmelerine ve tam potansiyellerine ulaşmalarına yardımcı olmak
+              üzere çeşitli düzeylerde mesleki yeterlilik ve bütünsel gelişime
+              dayalı sertifika programları sunmaktadır.
             </p>
           </div>
           <hr className="my-4" />
-          <h3 className="text-accent">Metasistemik Çalışmanın Farkı</h3>
+          <h3 className="text-accent">Eğitim Programlarımızla:</h3>
           <ul className="xl:px-4 my-4 xl:list-disc [&>li]:text-sm [&>li]:my-2 [&>li]:leading-6">
             <li>Bireysel yetenek ve beceri geliştirmenin ötesine geçiyoruz.</li>
             <li>
@@ -39,8 +39,9 @@ export default function SubHero({ img }) {
               geliştiriyoruz.
             </li>
             <li>
-              Kapsamlı mesleki yeterlilik ve gelişim sertifika programlarımız
-              kişisel gelişim ve sistemik etki için dönüştürücü bir alan sağlar.
+              Kapsamlı mesleki yeterlilik ve gelişim sertifika programlarımızla,
+              kişisel gelişim ve sistemik etki için dönüştürücü bir alan
+              sağlıyoruz.
             </li>
           </ul>
         </div>
